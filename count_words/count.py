cf64b762e3b1a1f1eba2c5a2cd455458fa0f0544
Marissa <Marissa@UtterbergDataDev.com>
2018-09-19 08:37:05 -0400

    Add files via upload

---
+++ count_words/count.py
@@ -0,0 +1,13 @@
+''' /Users/NothingToLose/Desktop/DevOps_and_DataProg/Python/count_words '''
+
+def count_words(input_str):
+    ''' Returns a dictionary with counts of each word in a string '''
+    words = input_str.split(' ')
+    slimwords = []
+    worddict = {}
+    for word in words:
+        if word not in slimwords:
+            slimwords.append(word)
+    for word in slimwords:
+        worddict[word] = words.count(word)
+    return worddict
