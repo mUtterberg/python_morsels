c9f74e3c055c19bf7399cddc608352beaae8e3de
Marissa <Marissa@UtterbergDataDev.com>
2018-10-02 19:35:19 -0400

    Update and rename add.py to add_v0.py

--- add/add_v0.py
+++ add/add_v0.py
@@ -3,13 +3,12 @@ def add(matrix1, matrix2):
     Takes two lists of lists.
     Returns one element-wise summed list of lists.
     '''
-    shape = matrix1
     sum = []
-    j = 0
+    i = 0
     for row in matrix1:
         working_row = []
-        for i in range(len(matrix1[j])):
-            working_row.append(matrix1[j][i] + matrix2[j][i])
+        for j in range(len(matrix1[i])):
+            working_row.append(matrix1[i][j] + matrix2[i][j])
         sum.append(working_row)
-        j += 1
+        i += 1
     return sum
