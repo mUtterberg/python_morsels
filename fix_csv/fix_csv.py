b6c16bc5bdd6192e407098b208cc7b325cfa7900
Marissa Utterberg <marissa@utterbergdatadev.com>
2020-05-06 16:45:40 -0400

    Update solution to clear Bonus #2

--- fix_csv/fix_csv.py
+++ fix_csv/fix_csv.py
@@ -5,23 +5,32 @@ import csv
 parser = argparse.ArgumentParser()
 parser.add_argument('in_csv')
 parser.add_argument('out_csv')
+parser.add_argument('--in-delimiter', dest="delim")
+parser.add_argument('--in-quote', dest="quote")
 
 
-def main(in_csv, out_csv):
-    with open(in_csv, 'r') as in_file:
-        # reader = csv.reader(in_file)
-        in_lines = in_file.readlines()
+def main(args):
+    arguments = {}
+    if args.delim:
+        arguments['delimiter'] = args.delim
+    if args.quote:
+        arguments['quotechar'] = args.quote
+    with open(args.in_csv, 'r', newline='') as in_file:
 
-    in_lines = [
-        [element for element in row[:-1].split('|')]
-        for row in in_lines
-    ]
+        # in_reader = csv.reader(in_file, delimiter=in_delim, quotechar=in_quote)
 
-    with open(out_csv, 'w', newline='') as out_file:
+        if not (args.delim or args.quote):
+            arguments['dialect'] = csv.Sniffer().sniff(in_file.read())
+            in_file.seek(0)
+
+        in_reader = csv.reader(in_file, **arguments)
+        in_lines = list(in_reader)
+
+    with open(args.out_csv, 'w', newline='') as out_file:
         writer = csv.writer(out_file)
         writer.writerows(in_lines)
 
 
 if __name__ == "__main__":
     cli_args = parser.parse_args()
-    main(cli_args.in_csv, cli_args.out_csv)
+    main(cli_args)
