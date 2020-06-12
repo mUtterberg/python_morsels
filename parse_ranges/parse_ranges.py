dcd0c404d530e6926f48d8f590e2becdde998a4c
mutterberg <Marissa@UtterbergDataDev.com>
2020-06-12 15:06:22 -0400

    Submitted solution.

--- parse_ranges/parse_ranges.py
+++ parse_ranges/parse_ranges.py
@@ -4,6 +4,10 @@ def parse_ranges(in_str):
     ranges = in_str.split(',')
     for x in ranges:
         start_range = int(x.split('-')[0].strip())
-        end_range = int(x.split('-')[-1].strip())+1
+        try:
+            end_range = int(x.split('-')[-1].strip())+1
+        except ValueError as val_e:
+            print(val_e)
+            end_range = start_range + 1
         for z in range(start_range,end_range):
             yield z
