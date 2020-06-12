68de62cd8378769984a1860868ed0b5c4484f8fa
mutterberg <Marissa@UtterbergDataDev.com>
2020-06-12 15:19:36 -0400

    Bonus 1

--- meetup_date/meetup_date.py
+++ meetup_date/meetup_date.py
@@ -1,12 +1,11 @@
 import datetime
 
 
-def meetup_date(year, month):
+def meetup_date(year, month, nth=4, weekday=3):
     first_date = datetime.date(year, month, 1)
-    thursday = 3
     day_of_week = first_date.weekday()
-    day_diff = 3 - day_of_week
+    day_diff = weekday - day_of_week
     if day_diff < 0:
         day_diff = day_diff + 7
-    meeting_date = first_date + datetime.timedelta(days=day_diff, weeks=3)
+    meeting_date = first_date + datetime.timedelta(days=day_diff, weeks=nth-1)
     return meeting_date
