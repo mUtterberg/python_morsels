8b000ec9b8b9e5f830209b14e64d53cc2c192658
mutterberg <Marissa@UtterbergDataDev.com>
2020-06-12 15:18:16 -0400

    Update day_diff

--- meetup_date/meetup_date.py
+++ meetup_date/meetup_date.py
@@ -5,11 +5,8 @@ def meetup_date(year, month):
     first_date = datetime.date(year, month, 1)
     thursday = 3
     day_of_week = first_date.weekday()
-    if day_of_week < 3:
-        day_diff = 3 - day_of_week
-    elif day_of_week == 3:
-        day_diff = 0
-    else:
-        day_diff = 3 - day_of_week + 7
+    day_diff = 3 - day_of_week
+    if day_diff < 0:
+        day_diff = day_diff + 7
     meeting_date = first_date + datetime.timedelta(days=day_diff, weeks=3)
     return meeting_date
