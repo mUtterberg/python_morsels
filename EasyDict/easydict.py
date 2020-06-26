fea5e573d84bd1608aa6873cbd09df559486e4c6
mutterberg <Marissa@UtterbergDataDev.com>
2020-06-26 14:06:13 -0400

    Complete bonuses

--- EasyDict/easydict.py
+++ EasyDict/easydict.py
@@ -1,15 +1,32 @@
 
 
 class EasyDict:
-    def __init__(self, easy_dict={}):
-        for item in easy_dict:
-            self.__setattr__(item, easy_dict[item])
+    def __init__(self, easy_dict={}, normalize=False, **kwargs):
+        self._normalize = normalize
+        for key, value in easy_dict.items():
+            self[key] = value
+        for key, value in kwargs.items():
+            self[key] = value
 
     def __getitem__(self, attr):
-        return self.__getattribute__(attr)
+        return getattr(self, self.normalized(attr))
 
     def __setitem__(self, attr, val):
-        return self.__setattr__(attr, val)
+        setattr(self, self.normalized(attr), val)
+    
+    def __eq__(self, easy_dict):
+        return self.__dict__ == easy_dict.__dict__
+
+    def normalized(self, key):
+        if self._normalize:
+            return key.replace(' ', '_')
+        return key
+    
+    def get(self, attr, default_value=None):
+        try:
+            return getattr(self, self.normalized(attr))
+        except AttributeError:
+            return default_value
 
 
 if __name__ == "__main__":
