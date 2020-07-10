4c30a23a42433e51b9005e1e48c441b768b068a7
Marissa <Marissa@UtterbergDataDev.com>
2020-07-10 15:33:44 -0400

    Remove unused functions from initial solution.

--- deep_add/deep_add.py
+++ deep_add/deep_add.py
@@ -1,31 +1,5 @@
 
 
-def is_int(input_elem):
-    if isinstance(input_elem, int):
-        return input_elem
-    return
-
-
-def has_value(input_elem):
-    if not input_elem:
-        return None
-    return input_elem
-
-
-def add_static(nested_list, output_sum):
-    next_elem = nested_list.pop()
-    try:
-        output_sum += next_elem
-    except TypeError as type_e:
-        if 'NoneType' in type_e.args[0]:
-            raise type_e
-        try:
-            nested_list += next_elem
-        except TypeError:
-            nested_list += list(next_elem)
-    return output_sum
-
-
 def deep_add(nested_list, start: int = 0):
     output_sum = start
     working_list = list(nested_list)
