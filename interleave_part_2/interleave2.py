ed4bf9964f0da26dd01ffff26f779fdc16620346
Marissa <Marissa@UtterbergDataDev.com>
2020-09-25 14:36:56 -0400

    Add interesting alternative solution below mine.

--- interleave_part_2/interleave2.py
+++ interleave_part_2/interleave2.py
@@ -10,6 +10,17 @@ def interleave(*iterables):
             if item is not sentinel:
                 yield item
 
+# def interleave(*iterables):
+#     """Iterate over input iterables"""
+
+#     iterators = [iter(i) for i in iterables]
+#     while iterators:
+#         for iterator in list(iterators):
+#             try:
+#                 yield next(iterator)
+#             except StopIteration:
+#                 iterators.remove(iterator)
+
 
 if __name__ == "__main__":
     print(list(interleave([1, 2], [3,], [4, 5, 6])))
