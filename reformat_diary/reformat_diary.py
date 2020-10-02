36d5b5166a48127b788eaa71d919e936d393488b
Marissa <Marissa@UtterbergDataDev.com>
2020-10-02 14:48:16 -0400

    Update solution to reformat_diary

--- reformat_diary/reformat_diary.py
+++ reformat_diary/reformat_diary.py
@@ -1,8 +1,7 @@
 import datetime
-# import re
 from io import StringIO
 from pathlib import Path
-from typing import IO, List, Tuple
+from typing import IO, Iterator, Tuple
 
 
 def clean_html(in_text: str) -> str:
@@ -10,44 +9,34 @@ def clean_html(in_text: str) -> str:
     return in_text.strip().replace('&nbsp;', ' ').replace('&quot;', '\"').replace('&amp;', '&')
 
 
-def entries_by_date(in_stream: IO) -> List[Tuple[str, str]]:
+def entries_by_date(in_stream: IO) -> Iterator[Tuple[str, str]]:
     """Formats a diary text file as tuples containing the date & diary entry"""
 
-    entries = []
-    this_entry = ['', '']  # type: List[str]
-    all_lines = in_stream.readlines()
-    n_lines = len(all_lines)
-    this_line = 0
-    while this_line < n_lines:
-        line = all_lines[this_line]
+    date = None
+    entry = ''
+    for line in in_stream:
         try:
-            datetime.datetime.strptime(line[:-1], '%Y-%m-%d')
-            if this_entry != ['', '']:
-                this_entry[-1] = clean_html(this_entry[-1])
-                entries.append(tuple(this_entry))
-            this_entry = [line[:-1], '']
-            this_line += 2
+            datetime.datetime.strptime(line.strip(), '%Y-%m-%d')
+            if date:
+                yield date, clean_html(entry)
+            date, entry = line.strip(), ''
         except ValueError:
-            this_entry[1] += line
-            this_line += 1
-    this_entry[-1] = clean_html(this_entry[-1])
-    entries.append(tuple(this_entry))
-    # in_text = in_stream.read()
-    # date_text = re.compile(r'')
-
-    return entries
+            entry += line
+    if date is None:
+        raise Exception('Diary is empty')
+    yield date, clean_html(entry)
 
 
 def main(f_name: str) -> None:
     """Accept diary filename & create files for each diary entry."""
     with Path(f_name).open('r') as in_stream:
         diary = entries_by_date(in_stream)
-    for entry in diary:
-        out_name, out_text = entry
-        out_name = f'{out_name}.txt'
-        with Path(out_name).open('w') as out_file:
-            out_file.write(out_text)
-            print(f'{out_name} written')
+        for entry in diary:
+            out_name, out_text = entry
+            out_name = f'{out_name}.txt'
+            with Path(out_name).open('w') as out_file:
+                out_file.write(out_text)
+                print(f'{out_name} written')
 
 
 if __name__ == "__main__":
