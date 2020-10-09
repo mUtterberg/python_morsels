9709fa0cd899bd9c4e0c793c2aa3054a2022f95f
Marissa <Marissa@UtterbergDataDev.com>
2020-10-09 13:59:07 -0400

    Use zip instead of index to assign widths, alignments

--- format_fixed_width/format_fixed_width.py
+++ format_fixed_width/format_fixed_width.py
@@ -1,17 +1,16 @@
-def format_fixed_width(in_list, padding=2, widths=None, alignments=None) -> str:
+def format_fixed_width(rows, padding=2, widths=None, alignments=None) -> str:
     """Formats a list of lists as a fixed-width formatted string"""
-    if not in_list:
+    if not rows:
         return ''
 
-    cols = max([len(nested_list) for nested_list in in_list])
     if widths is None:
         widths = [
-            max([len(nested_list[i]) for nested_list in in_list])
-            for i in range(cols)
+            max(len(cell) for cell in col)
+            for col in zip(*rows)
         ]
-    
+
     if alignments is None:
-        alignments = ['L' for i in range(cols)]
+        alignments = ['L' for col in zip(*rows)]
 
     pad = ' ' * padding
     return '\n'.join(
@@ -21,9 +20,9 @@ def format_fixed_width(in_list, padding=2, widths=None, alignments=None) -> str:
                     item.rjust(widths[i], ' ')
                     if alignments[i] == 'R'
                     else item.ljust(widths[i], ' ')
-                    for i, item in enumerate(nested_list)
+                    for i, item in enumerate(row)
                 ]
-            ).rstrip() for nested_list in in_list
+            ).rstrip() for row in rows
         ]
     )
 
