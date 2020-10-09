161fa6d1d9dc10de9ea6e7d78dab7861c97b723c
Marissa <Marissa@UtterbergDataDev.com>
2020-10-09 13:44:18 -0400

    Initial solution

---
+++ format_fixed_width/format_fixed_width.py
@@ -0,0 +1,24 @@
+def format_fixed_width(in_list, widths=None) -> str:
+    """Formats a list of lists as a fixed-width formatted string"""
+    if not in_list:
+        return ''
+
+    if widths is None:
+        widths = [
+            max([len(nested_list[i]) for nested_list in in_list])
+            for i in range(max([len(nested_list) for nested_list in in_list]))
+        ]
+
+    return '\n'.join(
+        [
+            '  '.join(
+                [
+                    item.ljust(widths[i], ' ') for i, item in enumerate(nested_list)
+                ]
+            ).rstrip() for nested_list in in_list
+        ]
+    )
+
+
+if __name__ == "__main__":
+    pass
