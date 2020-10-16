3a566d12b15abcb23e697118253eb90fa5e4b346
mutterberg <Marissa@UtterbergDataDev.com>
2020-10-16 18:00:55 -0400

    SHLEX????? Love it.

--- tags_equal/tags_equal.py
+++ tags_equal/tags_equal.py
@@ -1,16 +1,21 @@
 # pylint: disable=missing-module-docstring
 
 
-from typing import List
+import shlex
+from typing import Dict
 
 
-def validate_tag_and_strip(is_tag: str) -> List[str]:
+def validate_tag_and_strip(is_tag: str) -> Dict[str, str]:
     """Validate & strip outer syntax"""
     if is_tag[0] != '<':
         raise SyntaxError('HTML tag must open with "<"')
     if is_tag[-1] != '>':
         raise SyntaxError('HTML tag must close with ">"')
-    return sorted(is_tag[1:-1].lower().split(' '))
+    tags = reversed(shlex.split(is_tag[1:-1].lower()))
+    return {
+        x.split('=')[0]:x.split('=')[-1]
+        for x in tags
+        }
 
 
 def tags_equal(html_a: str, html_b: str) -> bool:
@@ -18,11 +23,15 @@ def tags_equal(html_a: str, html_b: str) -> bool:
     equal = True
     tag_a = validate_tag_and_strip(html_a)
     tag_b = validate_tag_and_strip(html_b)
-    for a, b in zip(tag_a, tag_b):
-        if a != b:
+    for a, b in zip(sorted(tag_a), sorted(tag_b)):
+        print(a, tag_a[a], b, tag_b[b])
+        if (a != b) or (tag_a[a] != tag_b[b]):
             equal = False
     return equal
 
 
 if __name__ == "__main__":
-    print(validate_tag_and_strip('<HTML '))
+    print(tags_equal(
+        '<label for=id_email for=id_username>',
+        '<label for=id_email>'
+    ))
