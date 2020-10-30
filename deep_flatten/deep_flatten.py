db4d647bd11cbd042873fbfb1be403d691fe0301
Marissa <Marissa@UtterbergDataDev.com>
2020-10-30 12:11:06 -0400

    Initial solution to deep_flatten and bonuses

--- deep_flatten/deep_flatten.py
+++ deep_flatten/deep_flatten.py
@@ -1,7 +1,29 @@
-def deep_flatten():
-    """"""
-    return
+from typing import Iterable, Iterator
+
+
+def deep_flatten(flatten_me: Iterable) -> Iterator:
+    """Flatten an iterable"""
+
+    for item in flatten_me:
+        if isinstance(item, str):
+            yield item
+            continue
+        try:
+            for inner in deep_flatten(item):
+                yield inner
+        except TypeError:
+            yield item
 
 
 if __name__ == "__main__":
-    pass
+    print(next(iter([2, 1])))
+
+    DEMO = [0, [1, [2, 3]], [4]]
+    print(list(deep_flatten(DEMO)))
+
+    print()
+    NUM_WORDS = enumerate([99, 98, 97])
+    FLAT = deep_flatten(NUM_WORDS)
+    print(next(FLAT))
+    print(next(FLAT))
+    print(next(NUM_WORDS))
