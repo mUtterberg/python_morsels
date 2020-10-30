a6de2ea3bd03751e1a021e63cd05e4a1b416b484
Marissa <Marissa@UtterbergDataDev.com>
2020-10-30 11:30:15 -0400

    Shorten code with identity key

--- minmax/minmax.py
+++ minmax/minmax.py
@@ -7,37 +7,28 @@ class MinMax(NamedTuple):
     max: Any
 
 
-def minmax(in_obj: Iterable, *, key: Callable = None) -> MinMax:
+def identity(obj: Any) -> Any:
+    """Identity as default key"""
+    return obj
+
+
+def minmax(in_obj: Iterable, *, key: Callable = identity) -> MinMax:
     """Find min & max of list"""
 
+    trans_min = None
+    trans_max = None
     min_obj = None
     max_obj = None
 
-    if key is None:
-        for next_obj in in_obj:
-            if (min_obj is None) or (next_obj < min_obj):
-                min_obj = next_obj
+    for next_obj in in_obj:
+        trans_obj = key(next_obj)
+        if (trans_min is None) or (trans_obj < trans_min):
+            trans_min = trans_obj
+            min_obj = next_obj
 
-            if (max_obj is None) or (next_obj > max_obj):
-                max_obj = next_obj
-
-    else:
-
-        trans_obj = iter(key(x) for x in in_obj)
-
-        min_orig = None
-        max_orig = None
-        for next_obj, next_orig in zip(trans_obj, in_obj):
-            if (min_obj is None) or (next_obj < min_obj):
-                min_obj = next_obj
-                min_orig = next_orig
-
-            if (max_obj is None) or (next_obj > max_obj):
-                max_obj = next_obj
-                max_orig = next_orig
-
-        min_obj = min_orig
-        max_obj = max_orig
+        if (max_obj is None) or (trans_obj > trans_max):
+            trans_max = trans_obj
+            max_obj = next_obj
 
     if (min_obj is None) or (max_obj is None):
         raise ValueError('Empty iterable')
