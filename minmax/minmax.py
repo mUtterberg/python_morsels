c3c50b6e504d638421ed8eedca4b2cd153bda058
Marissa <Marissa@UtterbergDataDev.com>
2020-10-30 13:48:09 -0400

    Assign starting min and max from next(iter)

--- minmax/minmax.py
+++ minmax/minmax.py
@@ -7,31 +7,29 @@ class MinMax(NamedTuple):
     max: Any
 
 
-def identity(obj: Any) -> Any:
-    """Identity as default key"""
-    return obj
-
-
-def minmax(in_obj: Iterable, *, key: Callable = identity) -> MinMax:
+def minmax(in_obj: Iterable, *, key: Callable = None) -> MinMax:
     """Find min & max of list"""
 
-    trans_min = None
-    trans_max = None
-    min_obj = None
-    max_obj = None
+    iter_obj = iter(in_obj)
 
-    for next_obj in in_obj:
-        trans_obj = key(next_obj)
-        if (trans_min is None) or (trans_obj < trans_min):
+    try:
+        min_obj = max_obj = next(iter_obj)
+        trans_min = trans_max = key(min_obj) if key else min_obj
+
+    except StopIteration as stop_iter_e:
+        raise ValueError('Empty iterable') from stop_iter_e
+
+    for obj in iter_obj:
+
+        trans_obj = key(obj) if key else obj
+
+        if trans_obj < trans_min:
             trans_min = trans_obj
-            min_obj = next_obj
+            min_obj = obj
 
-        if (max_obj is None) or (trans_obj > trans_max):
+        if trans_obj > trans_max:
             trans_max = trans_obj
-            max_obj = next_obj
-
-    if (min_obj is None) or (max_obj is None):
-        raise ValueError('Empty iterable')
+            max_obj = obj
 
     return MinMax(min_obj, max_obj)
 
