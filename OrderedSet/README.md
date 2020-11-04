efa6cb6109c84780a8554371756af4c85c656edc
Marissa <Marissa@UtterbergDataDev.com>
2020-11-04 14:43:50 -0500

    Initial solution to OrderedSet and bonuses

---
+++ OrderedSet/README.md
@@ -0,0 +1,9 @@
+# OrderedSet
+
+[Public repo with my work](https://github.com/mUtterberg/python_morsels/tree/master/orderedset/) for Python Morsels "OrderedSet" exercise, received November 2, 2020.
+
+This exercise was created by [Trey Hunner](https://treyhunner.com/) for [Python Morsels](https://try.pythonmorsels.com/).
+
+[Return to my Python Morsels](https://mutterberg.github.io/python_morsels)
+
+[Return to #100DaysOfCode Project Home](https://mutterberg.github.io)
---
+++ OrderedSet/orderedset.py
@@ -0,0 +1,49 @@
+from collections import OrderedDict
+from typing import Any, Iterable
+
+
+class OrderedSet:
+    """Like a set, but ordered!"""
+
+    def __init__(self, iter_in: Iterable = None) -> None:
+        if iter_in is None:
+            iter_in = []
+        self.data = OrderedDict.fromkeys(iter_in)
+
+    def __iter__(self) -> Iterable:
+        for item in self.data:
+            yield item
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __repr__(self) -> str:
+        return f'OrderedSet({list(self.data.keys())})'
+
+    def __contains__(self, item: Any) -> bool:
+        try:
+            self.data[item]
+            return True
+        except KeyError:
+            return False
+
+    def __eq__(self, o: object) -> bool:
+        try:
+            return self.data == o.data
+        except AttributeError:
+            return o == set(self.data)
+
+    def add(self, item: Any) -> None:
+        """Add unique data to self.data"""
+        self.data.setdefault(item)
+
+    def discard(self, item: Any) -> None:
+        """Remove data from self.data"""
+        self.data.pop(item, None)
+
+
+if __name__ == "__main__":
+    TEST_ITER = ['these', 'words', 'are', 'in', 'an', 'order']
+    print(*OrderedSet(TEST_ITER))
+
+    print(OrderedSet('abc') == OrderedSet('bac'))
