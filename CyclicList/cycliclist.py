1d5884c20cdab0ba16ea23b65a9d75921b1c0066
Marissa <Marissa@UtterbergDataDev.com>
2020-12-04 16:18:12 -0500

    Streamline use of super class UserList

--- CyclicList/cycliclist.py
+++ CyclicList/cycliclist.py
@@ -1,42 +1,25 @@
 from collections import UserList
-from itertools import cycle, islice
-from typing import Any, Iterable, List, Union
+from itertools import islice
+from typing import Any, Tuple, Union
 
 
-class CyclicList(UserList):
+class CyclicList(UserList):  # pylint: disable=too-many-ancestors
     """Sort of like a list except that looping over it will result in an infinite loop."""
-
-    def __init__(self, iter_input: Iterable[Any]) -> None:
-        super().__init__(iter_input)
-        self._data = iter_input
-        # self.data = iter_input
-
-    def __iter__(self) -> Any:
-        this_iter = cycle(self._data)
-        for item in this_iter:
-            yield item
-
-    def __len__(self) -> int:
-        return len(self._data)
 
     def __getitem__(self, index_val: Union[int, slice]):
         if isinstance(index_val, slice):
-            return self.__slice__(index_val.start, index_val.stop)
-        try:
-            return self._data[index_val]
-        except IndexError:
-            return self._data[index_val % len(self._data)]
+            return [self[i] for i in range(*self._slice_indices(index_val))]
+        return self.data[index_val % len(self)]
 
     def __setitem__(self, index_val: int, item: Any):
-        try:
-            self._data[index_val] = item
-        except IndexError:
-            self._data[index_val % len(self._data)] = item
+        self.data[index_val % len(self)] = item
 
-    def __slice__(self, start: int, stop: int) -> List[Any]:
+    def _slice_indices(self, obj: slice) -> Tuple[int, int, int]:
 
-        if not any((start, stop)):
-            return self._data[:]
+        if obj.step is not None:
+            raise ValueError('Step not supported.')
+
+        start, stop = obj.start, obj.stop
 
         if start is None:
             start = 0
@@ -44,26 +27,9 @@ class CyclicList(UserList):
             if start < 0:
                 stop = 0
             else:
-                stop = -1
+                stop = len(self)
 
-        if 0 <= start <= len(self._data):
-            if int(-1) <= stop <= len(self._data):
-                print('Returning standard slice')
-                return self._data[start:stop]
-            return [self[i] for i in range(start, stop)]
-        return [self[i] for i in range(start, stop)]
-
-
-    def append(self, new_item):
-        """Append data"""
-        self._data.append(new_item)
-
-    def pop(self, pop_index=None):
-        """Pop"""
-        try:
-            return self._data.pop(pop_index)
-        except TypeError:
-            return self._data.pop()
+        return start, stop, 1
 
 
 def run_local_review():
