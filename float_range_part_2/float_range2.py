7bb6e716864e91284a999110b1ec000253a4d810
Marissa <Marissa@UtterbergDataDev.com>
2021-01-08 17:29:09 -0500

    Bonuses for initial solution

--- float_range_part_2/float_range2.py
+++ float_range_part_2/float_range2.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Iterable, Iterator, List, Union
 
 
 class float_range:
@@ -33,8 +33,11 @@ class float_range:
             this_step += self._step
 
     def __next__(self):
-        for step in self.__iter__():
+        for step in iter(self):
             yield step
+
+    def __reversed__(self):
+        return iter(float_range(self.__getitem__(-1), self._start - self._step, self._step * -1))
 
     def _compare_step(self, this_step: float) -> bool:
         """Determine if element is in range"""
@@ -46,13 +49,45 @@ class float_range:
             return True
         return False
 
-    def __getitem__(self, in_dex: int) -> float:
+    def _get(self, in_dex: int) -> float:
+        """Get single index"""
         if in_dex < 0:
             in_dex += len(self)
         value = self._start + self._step * in_dex
         if not self._compare_step(value):
             raise IndexError
         return value
+
+    def __getitem__(self, item: Union[int, slice]) -> Union[List[float], float]:
+        if isinstance(item, int):
+            return self._get(item)
+        if isinstance(item, slice):
+            return_val = []
+            start = 0
+            stop = len(self)
+            stride = 1
+            if item.start is not None:
+                start = item.start
+                if start < 0:
+                    start = len(self) + item.start
+                    if start < 0:
+                        start = len(self)
+            if item.stop is not None:
+                stop = item.stop
+                if stop > len(self):
+                    stop = len(self)
+            if item.step is not None:
+                stride = item.step
+            for in_dex in range(start, stop, stride):
+                if in_dex < 0:
+                    in_dex += len(self)
+                value = self._start + self._step * in_dex
+                if not self._compare_step(value):
+                    raise IndexError
+                return_val.append(value)
+            return return_val
+        else:
+            raise TypeError
 
 
 if __name__ == "__main__":
