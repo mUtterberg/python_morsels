82659bb7bdd3f28b23ec5ffc8e69efd72ae26c9c
Marissa <Marissa@UtterbergDataDev.com>
2021-03-19 11:46:29 -0400

    Initial working solution for all bonuses

--- minmax_part_2/minmax2.py
+++ minmax_part_2/minmax2.py
@@ -1,15 +1,26 @@
-from typing import Any, Callable, Iterable, Tuple
+from typing import Any, Callable, Iterable, NamedTuple, Tuple, Union
 
 SENTINEL = object()
 
 
-def minmax(in_iter: Iterable[Any], *, default: Any = SENTINEL, key: Callable = lambda s: s) -> Tuple[Any, Any]:
-    """Not sure yet"""
+class MinMax(NamedTuple):
+    """Enable named attributes min & max"""
+    min: Any
+    max: Any
+
+
+def minmax(*in_iter: Tuple[Union[Any, Iterable[Any]]], default: Any = SENTINEL, key: Callable = lambda s: s) -> MinMax:
+    """Find min & max value in iterable"""
 
     if not in_iter:
-        if default == SENTINEL:
+        raise TypeError('minmax() has no args')
+    if len(in_iter) == 1:
+        in_iter = in_iter[0]
+
+    if not in_iter:
+        if default is SENTINEL:
             raise ValueError('minmax() arg is an empty iterable')
-        return default, default
+        return MinMax(default, default)
 
     trans_iter = [key(x) for x in in_iter]
     min_val = min(trans_iter)
@@ -21,8 +32,8 @@ def minmax(in_iter: Iterable[Any], *, default: Any = SENTINEL, key: Callable = l
         if key(val) == max_val:
             max_val = val
 
-    return min_val, max_val
+    return MinMax(min_val, max_val)
 
 
 if __name__ == '__main__':
-    pass
+    print(minmax())
