568d2b628351b135735522ac87c739550a1c227c
Marissa <Marissa@UtterbergDataDev.com>
2021-04-01 14:14:37 -0400

    Update with solutions to all bonuses

--- PermaDict/permadict.py
+++ PermaDict/permadict.py
@@ -1,4 +1,5 @@
 from collections import UserDict
+from typing import Iterable, Mapping, Optional, Union
 
 
 SENTINEL = object()
@@ -7,10 +8,41 @@ SENTINEL = object()
 class PermaDict(UserDict):
     """A class has no function."""
 
-    def __setitem__(self, key, item) -> None:
-        if super().get(key, SENTINEL) is SENTINEL:
+    # def __init__(self, __dict: Optional[Mapping] = None, *, silent: bool = False) -> None:
+    def __init__(self, __dict: Optional[Mapping] = None, **kwargs) -> None:
+        # self.silent = silent
+        # self.force = force
+        data = dict(kwargs)
+
+        self.silent = data.pop('silent', False)
+
+        if __dict is None:
+            super().__init__(data)
+        else:
+            super().__init__(__dict)
+
+    def __setitem__(self, key, item, force: bool = False) -> None:
+        if force or super().get(key, SENTINEL) is SENTINEL:
             return super().__setitem__(key, item)
+        if self.silent:
+            return None
         raise KeyError(f'\'{key}\' already in dictionary')
+
+    def update(self, other=None, **kwargs):
+
+        force = kwargs.pop('force', False)
+
+        if other is None:
+            data = kwargs
+        else:
+            data = dict(other)
+
+        for key, value in data.items():
+            self.__setitem__(key, value, force)
+
+    def force_set(self, key, value) -> None:
+        """Force mutability of existing key-values"""
+        return super().__setitem__(key, value)
 
 
 def check_cases():
