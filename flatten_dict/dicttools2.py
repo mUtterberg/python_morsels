ec658a643698e161f336f21abd969980d980efee
Marissa <Marissa@UtterbergDataDev.com>
2021-04-16 14:39:30 -0400

    Bonus #1

--- flatten_dict/dicttools2.py
+++ flatten_dict/dicttools2.py
@@ -1,16 +1,25 @@
 from typing import Any, Dict
 
 
-def flatten_dict(in_obj: Dict[str, Dict[str, Any]], *, sep: str = '_') -> Dict[str, Any]:
+def flatten_dict(in_obj: Dict[str, Any], *, sep: str = '_') -> Dict[str, Any]:
     """Flattens a dict of dicts"""
 
     out_dict = {}
-    for key, in_dict in in_obj.items():
-        for in_key, value in in_dict.items():
-            out_dict[f'{key}{sep}{in_key}'] = value
+    for key, obj in in_obj.items():
+
+        try:
+            for inner_key, value in obj.items():
+                out_dict[f'{key}{sep}{inner_key}'] = value
+        except AttributeError:
+            out_dict[key] = obj
 
     return out_dict
 
 
+def run_examples() -> None:
+    """Run examples"""
+    print(flatten_dict({'vowels': {'a': 1, 'e': 2}, 'b': 4}))
+
+
 if __name__ == '__main__':
-    pass
+    run_examples()
