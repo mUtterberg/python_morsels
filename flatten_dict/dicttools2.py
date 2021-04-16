1a324ee4dc13acad1ebfffaa3ac47f237985f205
Marissa <Marissa@UtterbergDataDev.com>
2021-04-16 15:10:53 -0400

    Bonus #2

--- flatten_dict/dicttools2.py
+++ flatten_dict/dicttools2.py
@@ -9,7 +9,13 @@ def flatten_dict(in_obj: Dict[str, Any], *, sep: str = '_') -> Dict[str, Any]:
 
         try:
             for inner_key, value in obj.items():
-                out_dict[f'{key}{sep}{inner_key}'] = value
+                try:
+                    out_dict.update(
+                        flatten_dict(
+                            {sep.join([str(key), str(inner_key)]): value}, sep=sep)
+                        )
+                except AttributeError:
+                    out_dict[sep.join([key, inner_key])] = value
         except AttributeError:
             out_dict[key] = obj
 
@@ -19,6 +25,9 @@ def flatten_dict(in_obj: Dict[str, Any], *, sep: str = '_') -> Dict[str, Any]:
 def run_examples() -> None:
     """Run examples"""
     print(flatten_dict({'vowels': {'a': 1, 'e': 2}, 'b': 4}))
+
+    stuff = {'red': {'fruit': {'apple': 4}}, 'green': {'fruit': {'grape': 8}}}
+    print(flatten_dict(stuff, sep='/'))
 
 
 if __name__ == '__main__':
