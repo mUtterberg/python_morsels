1082d98305def38aa791218d99c08a98387b7a32
Marissa <Marissa@UtterbergDataDev.com>
2021-04-30 14:44:42 -0400

    Bonus problems

--- unsmart/unsmart.py
+++ unsmart/unsmart.py
@@ -3,18 +3,32 @@ import unicodedata
 from typing import IO
 
 
-def unsmarten(in_stream: IO) -> str:
-    """Actual conversion function"""
-    in_text = in_stream.read()
-    in_stream.close()
+def unsmarten_line(in_line: str) -> str:
+    """Single line"""
     out_text = ''
-    for cha in in_text:
+    for cha in in_line:
         if 'DOUBLE QUOTATION' in unicodedata.name(cha, 'f'):
             out_text += '"'
         elif 'SINGLE QUOTATION' in unicodedata.name(cha, 'f'):
             out_text += '\''
+        elif 'ELLIPS' in unicodedata.name(cha, 'f'):
+            out_text += '...'
+        elif 'EN DASH' in unicodedata.name(cha, 'f'):
+            out_text += '-'
+        elif 'EM DASH' in unicodedata.name(cha, 'f'):
+            out_text += '--'
         else:
             out_text += cha
+    return out_text.rstrip() + '\n'
+
+
+def unsmarten(in_stream: IO) -> str:
+    """Actual conversion function"""
+    in_text = in_stream.readlines()
+    in_stream.close()
+    out_text = ''
+    for line in in_text:
+        out_text += unsmarten_line(line)
     return out_text
 
 
