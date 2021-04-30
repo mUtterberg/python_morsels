7acbce41b26d26375c6d3c0dd858b80b3d811a5a
Marissa <Marissa@UtterbergDataDev.com>
2021-04-30 13:50:44 -0400

    Remove unused code

--- markdownify/markdownify.py
+++ markdownify/markdownify.py
@@ -8,7 +8,11 @@ class MarkdownifyParser(HTMLParser):
     processed_text = ''
     link_url = ''
 
-    def handle_starttag(self, tag: str, attrs: List[Tuple[str, Optional[str]]]) -> None:
+    def handle_starttag(
+            self,
+            tag: str,
+            attrs: List[Tuple[str, Optional[str]]]
+        ) -> None:
         if tag == 'br':
             self.processed_text = self.processed_text.strip() + '  \n'
         elif tag == 'strong':
@@ -32,13 +36,9 @@ class MarkdownifyParser(HTMLParser):
             self.processed_text += f']({self.link_url})'
             self.link_url = ''
 
-    def consolidate_processed(self) -> None:
-        """Whitespace normalization"""
-
     @property
     def markdown(self) -> str:
         """Convert processed lines to markdown string"""
-        # return '\n\n'.join(self.processed_lines)
         return self.processed_text.strip()
 
 
@@ -46,14 +46,13 @@ def markdownify(html_str: str) -> str:
     """Convert HTML to markdown"""
     parser = MarkdownifyParser()
     parser.feed(html_str)
-    parser.close()
     return parser.markdown
 
 
-def run_examples() -> None:
-    """Run some examples!"""
+def run_example() -> None:
+    """Run an example!"""
     print(markdownify('<a href="http://www.treyhunner.com">Trey</a> has a blog'))
 
 
 if __name__ == '__main__':
-    run_examples()
+    run_example()
