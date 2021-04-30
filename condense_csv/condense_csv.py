09266424ead659b1e8e0a489c878ca828c5e7520
Marissa <Marissa@UtterbergDataDev.com>
2021-04-30 15:16:14 -0400

    Initial solution for bonus problems 2 & 3 (not 1)

--- condense_csv/condense_csv.py
+++ condense_csv/condense_csv.py
@@ -1,7 +1,15 @@
-def condense_csv(in_stream: str, *, id_name: str) -> str:
+def condense_csv(in_stream: str, *, id_name: str = None) -> str:
     """Group CSV text by the first column"""
+
     details = {}
+
+    if id_name is None:
+        header, in_stream = in_stream.split('\n', 1)
+        header = header.split(',')
+        id_name = header[0]
+
     line_attrs = [id_name]
+
     for line in in_stream.split('\n'):
         line_id, line_attr, line_value = line.split(',')
         details.setdefault(line_id, {})
@@ -11,7 +19,7 @@ def condense_csv(in_stream: str, *, id_name: str) -> str:
 
     out_rows = [line_attrs]
     for line_id in details:
-        out_rows.append([line_id]+[details[line_id][line_attr] for line_attr in line_attrs[1:]])
+        out_rows.append([line_id]+[details[line_id].get(line_attr, '') for line_attr in line_attrs[1:]])
 
     return '\n'.join(','.join(out_row) for out_row in out_rows)
 
@@ -25,6 +33,15 @@ def run_example():
 02,Title,Honky Tonk Heroes (Like Me)
 02,Time,3:29'''
     print(condense_csv(sample, id_name='Track'))
+    print()
+    text = """object,property,value
+ball,color,purple
+ball,size,4
+ball,notes,it's round
+cup,color,blue
+cup,size,1
+cup,notes,none"""
+    print(condense_csv(text))
 
 
 if __name__ == '__main__':
